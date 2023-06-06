a63e4b79c2983b32c06cf9b0d5cef6e175295841
Jatin <j_chhikara@fanshaweonline.ca>
2023-06-05 23:07:48 -0400

    User Authentication: Develop the secure sign-in process for users based on their registered email and password.

--- App.js
+++ App.js
@@ -1,8 +1,12 @@
 import React from "react";
+import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
+
 import { NavigationContainer } from '@react-navigation/native';
+
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import TasksScreen from "./screens/TasksScreen";
+import Footer from "./components/footer";
 
 const Tab = createBottomTabNavigator();
 
@@ -10,9 +14,16 @@ const Tab = createBottomTabNavigator();
 export default function App() {
   return (
       <NavigationContainer>
-        <Tab.Navigator>
+        <Tab.Navigator
+                tabBar={(props) => {
+                  if (props.state.routeNames[props.state.index] !== 'Login' && props.state.routeNames[props.state.index] !== 'Register') {
+                    return <Footer {...props} />;
+                  }
+                }}>
+          <Tab.Screen name='Login' component={LoginScreen}/>
           <Tab.Screen name='Register' component={RegisterScreen} />
           <Tab.Screen name='Tasks' component={TasksScreen} />
+
         </Tab.Navigator>
       </NavigationContainer>
   );
---
+++ screens/LoginScreen.js
@@ -0,0 +1,116 @@
+import React, { useState } from 'react';
+import { View, Image, StyleSheet } from 'react-native';
+import { Input, Button, Text } from 'react-native-elements';
+import { useFocusEffect, useNavigation } from '@react-navigation/native';
+import { signInWithEmailAndPassword, sendPasswordResetEmail } from "firebase/auth";
+import { auth } from "../services/firebase";
+
+const LoginScreen = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+
+  const navigation = useNavigation();
+
+  useFocusEffect(
+    React.useCallback(() => {
+      return () => {
+        setEmail('');
+        setPassword('');
+      };
+    }, [])
+  );
+
+  const resetPassword = async () => {
+    await sendPasswordResetEmail(auth, email)
+      .then(() => {
+        alert('Password reset email sent!');
+      })
+      .catch((error) => {
+        console.error('Error sending password reset email:', error);
+      });
+  }
+
+
+  const handleRegister = () => {
+    navigation.navigate('Register');
+  }
+
+  const handleLogin = async () => {
+    try {
+      await signInWithEmailAndPassword(auth, email, password).then(() => {
+        navigation.navigate('Tasks');
+      })
+    } catch (error) {
+      alert('Incorrect email or password')
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+
+      <Image
+        source={require('../assets/taskify-logo.png')}
+        style={styles.logo}
+      />
+
+      <Input
+        placeholder="Email"
+        value={email}
+        onChangeText={setEmail}
+        rightIcon={{
+          type: 'font-awesome',
+          name: 'envelope'
+        }}
+      />
+
+      <Input
+        placeholder="Password"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry={!showPassword}
+        rightIcon={{
+          type: 'font-awesome',
+          name: showPassword ? 'eye-slash' : 'eye',
+          onPress: () => setShowPassword(!showPassword),
+        }}
+      />
+
+      <Text onPress={resetPassword} style={styles.forgotPasswordText}>Forgot Password?</Text>
+
+      <Button
+        title="Login"
+        onPress={handleLogin}
+      />
+
+      <Text style={styles.registerText}>
+        New user?{' '}
+        <Text onPress={handleRegister} style={styles.registerLink}>Register now</Text>
+      </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logo: {
+    width: 150,
+    height: 150,
+    marginBottom: 30,
+  },
+  forgotPasswordText: {
+    marginVertical: 10,
+  },
+  registerText: {
+    marginTop: 20,
+  },
+  registerLink: {
+    color: 'blue',
+  },
+});
+
+export default LoginScreen;
